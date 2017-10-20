e64ac378a43cbc52acbcfe8ecf288166a6c264e4
SandeepAmarnath <sandeep.amarnath@dal.ca>
2017-10-20 15:32:06 -0300

    This code seperates all the access list lines and puts to Access_list.cfg

---
+++ Access_lists.py
@@ -0,0 +1,9 @@
+
+#Creates a file which has access lists info
+with open('running-config.cfg','r') as run:
+    with open('Access_list.cfg','w') as new:
+
+        for line in run:
+            lines=run.readline()
+            if 'access-list' in lines:
+                new.write(lines)
