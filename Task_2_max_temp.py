27d21fb741482692e7403bc1d7fc085cb4948849
SandeepAmarnath <sandeep.amarnath@dal.ca>
2017-10-20 14:47:40 -0300

    Finding the maximim temparature

---
+++ Task_2_max_temp.py
@@ -0,0 +1,33 @@
+from weather import Weather
+weather = Weather()
+
+# Lookup via location name.
+
+location = weather.lookup_by_location('halifax')
+condition = location.condition()
+print (condition['text'])
+
+#Gives you all the days inside a list and the days will be of sublists
+all_days=[]
+
+i=0
+for forecasts in location.forecast():
+    if i<5:
+        daily = []
+        daily.append(forecasts['text'])
+        daily.append(forecasts['date'])
+        daily.append(forecasts['high'])
+        daily.append(forecasts['low'])
+        all_days.append(daily)
+        i+=1
+
+print(all_days)
+#Day has the highest temp
+maxT=0
+for day in all_days:
+    if int(day[2])>int(maxT):
+        maxT=day[2]
+print("The hottest day is on "+day[1] +'and the temparature is '+ maxT)
+
+
+
